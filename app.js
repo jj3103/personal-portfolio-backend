cbe16df537584dc243b2894233f553614c8a8b05
jj3103 <jainjayesh2003@gmail.com>
2025-04-04 18:33:11 +0530

    Some Url changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // allow your frontend during development
+    origin: "https://portfolio-green-eight-85.vercel.app", // allow your frontend during development
     methods: ["GET", "POST"],
     credentials: true,
   })
